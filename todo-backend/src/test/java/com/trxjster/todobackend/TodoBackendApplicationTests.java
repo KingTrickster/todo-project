399aba6222346aa001836c15256850106e55f596
Alejandro Trejo <alejandro.trejo@enroutesystems.com>
2022-07-31 16:10:05 -0500

    Added Backend project

---
+++ todo-backend/src/test/java/com/trxjster/todobackend/TodoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trxjster.todobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
